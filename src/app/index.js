fd8418209274d32faa2cc39d1b7462278c431eae
Omer Dogan <omerdogan3@gmail.com>
2017-09-18 19:53:43 +0300

    change headerlink

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,39 @@
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component{
+	
+	onGreet(){
+		alert("Hell!");
+	};
+
+
+
+	render() {
+		var user = {
+			name:"Melissa",
+			hobies: ["Sports", "Reading" , "OD"]
+		}
+
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Header/>
+					</div> 
+				</div>
+				<div className="row">
+					<div className="col-xs-10 col-xs-offset-1">
+						<Home name={"Max"} initialAge={27} user={user} greet={this.onGreet}/>
+						
+					</div> 
+				</div>
+			</div>
+		);
+	}
+}
+
+render(<App/>,window.document.getElementById('app'));
